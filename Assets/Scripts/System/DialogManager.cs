41b6937257a3ba78cf024e7e34258aa4330f5671
pearl0408 <roskfl000408@gmail.com>
2021-03-24 06:08:49 +0900

    게임 시작시 플레이어 랜덤 위치에 생성 기능 구현

---
+++ Assets/Scripts/System/DialogManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+
+    public Text startDialog;
+
+    //게임 시작시 메시지
+    private string[] startDialogText = {"미로의 모든 길을 지나는 즉시 출구가 열릴 것입니다.",
+                                    "괴물들을 피해 열린 출구로 탈출하십시오.",
+                                    "그럼 행운을 빕니다."};
+
+
+    void Start()
+    {
+        //게임 시작 메시지 출력
+        StartCoroutine(_typing());
+    }
+
+    IEnumerator _typing()
+    {
+        //메시지의 개수만큼 메시지 출력
+        for (int j = 0; j < startDialogText.Length; j++)
+        {
+            //첫 딜레이 설정(2초)
+            yield return new WaitForSeconds(3f);
+
+            //타이핑 효과는 각 text의 길이만큼 반복
+            for (int i = 0; i <= startDialogText[j].Length; i++)
+            {
+                //Substing(start index, 표현할 글자 개수) -> index부터 i까지의 글자를 표현 == 타이핑 효과
+                startDialog.text = startDialogText[j].Substring(0, i);
+
+                //한 번에 표현되지 않도록 딜레이를 줌
+                yield return new WaitForSeconds(0.15f);
+            }
+        }
+
+        //딜레이 설정
+        yield return new WaitForSeconds(3f);
+
+        //메시지 지우기
+        startDialog.text = "";
+    }
+}
---
+++ Assets/Scripts/System/SceneChange.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    void Start()
+    {
+        //모바일 해상도 고정 (1920, 1080으로 조정)
+        //1920 * 1080 / 1280 * 720
+        Screen.SetResolution(1920, 1080, true);
+        //모바일 해상도 고정(16:9로 고정)
+        Screen.SetResolution(Screen.width, Screen.width * 16 / 9, true);
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangeStartScene()
+    {
+        //start 버튼을 누르면 start 씬으로 이동
+        SceneManager.LoadScene ("Playing");
+    }
+
+    public void ChangeLankingScene()
+    {
+        //Lanking 버튼을 누르면 Lanking 씬으로 이동
+        SceneManager.LoadScene("Lanking");
+    }
+
+    public void Exit()
+    {
+        //Exit 버튼을 누르면 게임 종료
+        Application.Quit();
+    }
+}
