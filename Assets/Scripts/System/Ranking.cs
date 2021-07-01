77c45c142931b8790c00ac94e34a1625c5c1281e
정호진 <dnjfs@naver.com>
2021-07-02 05:01:03 +0900

    랭킹 구현중
    - 랭킹 씬에서 데이터베이스의 데이터를 불러 화면에 보여줌
    - 쓰레드 문제로 수정 필요

---
+++ Assets/Scripts/System/Ranking.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity;
+
+
+public class Ranking : MonoBehaviour
+{
+    public Text PageText;
+    public bool isEnableChild, created;
+
+    DatabaseReference reference;
+    List<IDictionary> UserRank = new List<IDictionary>();
+    int Page = 1;
+
+    void Start()
+    {
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        DataLoad();
+    }
+
+    private void DataLoad() //파이어베이스의 랭킹 불러오기
+    {
+        //reference의 자식(RANK)를 task로 받음
+        reference.Child("RANK").GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+                Debug.Log("error");
+            }
+            else if (task.IsCompleted) //task가 성공적이면
+            {
+                DataSnapshot snapshot = task.Result; //DataSnapshot 변수를 선언하여 task의 결과 값을 받음
+
+                Debug.Log("저장된 데이터 수: " + snapshot.ChildrenCount);
+
+                //List<IDictionary> UserRank = new List<IDictionary>();
+                foreach (DataSnapshot data in snapshot.Children) //데이터베이스에서 값들을 불러와 personInfo에 저장
+                    UserRank.Add((IDictionary)data.Value);
+
+                //점수와 시간으로 정렬
+                UserRank.Sort(delegate (IDictionary x, IDictionary y)
+                {
+                    string X = x["score"].ToString();
+                    string Y = y["score"].ToString();
+                    int xScore = int.Parse(X);
+                    int yScore = int.Parse(Y);
+
+                    if (xScore == yScore) //점수가 같은 경우
+                    {
+                        if (xScore == 0) //탈출 실패 시
+                            return int.Parse(x["time"].ToString()) < int.Parse(y["time"].ToString()) ? 1 : -1; //내림차순으로 정렬 (시간이 긴 순서대로)
+                        return int.Parse(x["time"].ToString()) < int.Parse(y["time"].ToString()) ? -1 : 1; //오름차순으로 정렬 (시간이 짧은 순서대로)
+                    }
+
+                    return int.Parse(X) < int.Parse(Y) ? 1 : -1; //내림차순으로 정렬 (점수가 높은 순서대로)
+                });
+
+                int rank = 1;
+                foreach (IDictionary temp in UserRank)
+                    Debug.Log(rank++ + "등 name: " + temp["username"] + ", score: " + temp["score"] + ", time: " + temp["time"]);
+
+                PageChange(); //쓰레드 밖에서 불러오기 (Task가 끝난 후)
+            }
+        });
+    }
+
+    public void OnClickLeft()
+    {
+        if (Page <= 1)
+            return;
+
+        Page--;
+        PageChange();
+        PageText.text = Page.ToString();
+    }
+    public void OnClickRight()
+    {
+        if (Page > UserRank.Count / 5)
+            return;
+
+        Page++;
+        PageChange();
+        PageText.text = Page.ToString();
+    }
+
+    private void PageChange()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject row = this.transform.GetChild(i).gameObject; //start에서 에러
+            int rank = (Page - 1) * 5 + i;
+            if (UserRank.Count - 1 < rank) //데이터의 수를 넘어가는 경우
+            {
+                row.SetActive(false);
+                continue;
+            }
+
+            row.SetActive(true);
+            if (i < 3)
+            {
+                if (Page == 1)
+                {
+                    row.transform.Find("Icon").gameObject.SetActive(true);
+                    row.transform.Find("Rank").gameObject.SetActive(false);
+                }
+                else
+                {
+                    row.transform.Find("Icon").gameObject.SetActive(false);
+                    row.transform.Find("Rank").gameObject.SetActive(true);
+                }
+            }
+            row.transform.Find("Rank").gameObject.GetComponent<Text>().text = (rank+1).ToString();
+            row.transform.Find("UserName").gameObject.GetComponent<Text>().text = UserRank[rank]["username"].ToString();
+            row.transform.Find("Score").gameObject.GetComponent<Text>().text = UserRank[rank]["score"].ToString();
+            row.transform.Find("Time").gameObject.GetComponent<Text>().text = UserRank[rank]["time"].ToString();
+        }
+    }
+}
