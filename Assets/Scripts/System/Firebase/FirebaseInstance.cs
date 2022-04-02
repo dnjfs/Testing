7d5c7cb3d18283f6e9e1969dc36e32e2a518c5b8
정호진 <dnjfs@naver.com>
2022-04-02 21:23:45 +0900

    구글 플레이 로그인 테스트
    - 구글 플레이 로그인 기능 구현
    - 구글 플레이 계정으로 파이어베이스 로그인 테스트 기능 추가

--- Assets/Scripts/System/Firebase/FirebaseInstance.cs
+++ Assets/Scripts/System/Firebase/FirebaseInstance.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 using Firebase;
 using Firebase.Auth;
 
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
 public class FirebaseInstance : MonoBehaviour
 {
-    //FirebaseApp app = null;
-    FirebaseAuth auth;
-    FirebaseUser user;
+    public UnityEngine.UI.Text googleAccountID;
+    string authCode;
 
     void Awake()
     {
@@ -19,6 +21,11 @@ public class FirebaseInstance : MonoBehaviour
 
     void Start()
     {
+        //구글게임 시작
+        PlayGamesPlatform.InitializeInstance(new PlayGamesClientConfiguration.Builder().Build());
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+
         //파이어베이스 시작
         Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
             var dependencyStatus = task.Result;
@@ -29,7 +36,8 @@ public class FirebaseInstance : MonoBehaviour
                 //app = Firebase.FirebaseApp.DefaultInstance;
 
                 // Set a flag here to indicate whether Firebase is ready to use by your app.
-                LoginAnonymous();
+
+                //LoginAnonymous();
             }
             else
             {
@@ -42,7 +50,7 @@ public class FirebaseInstance : MonoBehaviour
 
     void LoginAnonymous()
     {
-        auth = FirebaseAuth.DefaultInstance;
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
         //익명 로그인
         auth.SignInAnonymouslyAsync().ContinueWith(task => {
             if (task.IsCanceled)
@@ -58,14 +66,64 @@ public class FirebaseInstance : MonoBehaviour
                 return;
             }
 
-            user = task.Result;
+            Firebase.Auth.FirebaseUser newUser = task.Result;
             Debug.LogFormat("User signed in successfully: {0} ({1})",
-                user.DisplayName, user.UserId);
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    public void LoginGoogleAccount()
+    {
+        //구글계정으로 로그인
+        if (!Social.localUser.authenticated) // 로그인 되어 있지 않다면
+        {
+            Social.localUser.Authenticate((bool success) =>
+            {
+                if (success)
+                {
+                    authCode = PlayGamesPlatform.Instance.GetServerAuthCode();
+                    googleAccountID.text = Social.localUser.userName;
+                }
+                else
+                    googleAccountID.text = "Fail..";
+            });
+        }
+    }
+
+    public void LoginFirebase()
+    {
+        //파이어베이스 로그인
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        Firebase.Auth.Credential credential = Firebase.Auth.PlayGamesAuthProvider.GetCredential(authCode);
+        auth.SignInWithCredentialAsync(credential).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithCredentialAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+
+            googleAccountID.text = newUser.DisplayName;
         });
     }
 
     void OnDestroy()
     {
+        if (Social.localUser.authenticated)
+        {
+            PlayGamesPlatform.Instance.SignOut();
+            Firebase.Auth.FirebaseAuth.DefaultInstance.SignOut();
+        }
+        /*
+        Firebase.Auth.FirebaseUser user = Firebase.Auth.FirebaseAuth.DefaultInstance.CurrentUser;
         if (user != null)
         {
             user.DeleteAsync().ContinueWith(task =>
@@ -84,5 +142,6 @@ public class FirebaseInstance : MonoBehaviour
                 Debug.Log("User deleted successfully.");
             });
         }
+        */
     }
 }
